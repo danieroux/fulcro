8dd164f6dee0ddb04b2124a20ed783a0c2b9ec71
Danie Roux <danie@danieroux.com>
2025-07-07 01:30:35 +0200

    Change `nested-dynamic-routing-tree-cards` to have namespaced queries and idents, add `ensure-ident-can-be-derived` middleware for it, and enable console errors overlay
    - An intertwined commit that could be unwound

--- src/workspaces/com/fulcrologic/fulcro/cards/nested_dynamic_routing_tree_cards.cljs
+++ src/workspaces/com/fulcrologic/fulcro/cards/nested_dynamic_routing_tree_cards.cljs
@@ -1,68 +1,72 @@
 (ns com.fulcrologic.fulcro.cards.nested-dynamic-routing-tree-cards
   (:require
+    [com.fulcrologic.fulcro.cards.console-errors-display]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.dom :as dom]
     [com.fulcrologic.fulcro.react.hooks :as hooks]
     [com.fulcrologic.fulcro.routing.dynamic-routing :as dr :refer [defrouter]]
     [com.fulcrologic.fulcro.react.version18 :as react18]
+    [com.fulcrologic.fulcro.components :as comp]
+    [goog.object :as gobj]
     [nubank.workspaces.card-types.fulcro3 :as ct.fulcro]
     [nubank.workspaces.core :as ws]
     [taoensso.timbre :as log]))
 
-(defsc A1 [this {:keys [:id] :as props}]
-  {:query               [:id]
+(defsc A1 [this {:keys [:a1/id] :as props}]
+  {:query               [:a1/id]
    :route-segment       ["a1"]
    :will-enter          (fn [app params]
                           (log/info "A1 will enter")
-                          (dr/route-deferred [:id "a1"]
-                            (fn [] (js/setTimeout #(dr/target-ready! app [:id "a1"]) 300))))
+                          (dr/route-deferred [:a1/id "a1"]
+                            (fn [] (js/setTimeout #(dr/target-ready! app [:a1/id "a1"]) 300))))
    :will-leave          (fn [cls props] (log/info "A1 will leave"))
    :allow-route-change? (fn [c] (log/info "A1 allow route change?") true)
-   :initial-state       {:id "a1"}
-   :ident               :id}
+   :initial-state       {:a1/id "a1"}
+   :ident               :a1/id}
   (let [parent comp/*parent*]
     (dom/div "A1"
       (dom/button
-        {:onClick #(dr/change-route-relative! this this [:.. "a2"])}
+        {:onClick   #(dr/change-route-relative! this this [:.. "a2"])
+         :className "router-button"}
         "Go to sibling A2"))))
 
-(defsc A2 [this {:keys [:id] :as props}]
-  {:query               [:id]
+(defsc A2 [this {:keys [:a2/id] :as props}]
+  {:query               [:a2/id]
    :route-segment       ["a2"]
    :will-enter          (fn [app params]
                           (log/info "A2 will enter")
-                          (dr/route-deferred [:id "a2"]
-                            (fn [] (js/setTimeout #(dr/target-ready! app [:id "a2"]) 300))))
+                          (dr/route-deferred [:a2/id "a2"]
+                            (fn [] (js/setTimeout #(dr/target-ready! app [:a2/id "a2"]) 300))))
    :will-leave          (fn [cls props] (log/info "A2 will leave"))
    :allow-route-change? (fn [c] (log/info "A2 allow route change?") true)
-   :initial-state       {:id "a2"}
-   :ident               :id}
+   :initial-state       {:a2/id "a2"}
+   :ident               :a2/id}
   (dom/div "A2"))
 
-(defsc B1 [this {:keys [:id] :as props}]
-  {:query               [:id]
+(defsc B1 [this {:keys [:b1/id] :as props}]
+  {:query               [:b1/id]
    :route-segment       ["b1"]
    :will-enter          (fn [app params]
                           (log/info "B1 will enter")
-                          (dr/route-deferred [:id "b1"]
-                            (fn [] (js/setTimeout #(dr/target-ready! app [:id "b1"]) 300))))
+                          (dr/route-deferred [:b1/id "b1"]
+                            (fn [] (js/setTimeout #(dr/target-ready! app [:b1/id "b1"]) 300))))
    :will-leave          (fn [cls props] (log/info "B1 will leave"))
    :allow-route-change? (fn [c] (log/info "B1 allow route change?") true)
-   :initial-state       {:id "b1"}
-   :ident               :id}
+   :initial-state       {:b1/id "b1"}
+   :ident               :b1/id}
   (dom/div "B1"))
 
-(defsc B2 [this {:keys [:id] :as props}]
-  {:query               [:id]
+(defsc B2 [this {:keys [:b2/id] :as props}]
+  {:query               [:b2/id]
    :route-segment       ["b2"]
    :will-enter          (fn [app params]
                           (log/info "B2 will enter")
-                          (dr/route-deferred [:id "b2"]
-                            (fn [] (js/setTimeout #(dr/target-ready! app [:id "b2"]) 300))))
+                          (dr/route-deferred [:b2/id "b2"]
+                            (fn [] (js/setTimeout #(dr/target-ready! app [:b2/id "b2"]) 300))))
    :will-leave          (fn [cls props] (log/info "B2 will leave"))
    :allow-route-change? (fn [c] (log/info "B2 allow route change?") true)
-   :initial-state       {:id "b2"}
-   :ident               :id}
+   :initial-state       {:b2/id "b2"}
+   :ident               :b2/id}
   (dom/div "B2"))
 
 (defrouter ARouter [this props]
@@ -73,41 +77,41 @@
   {:router-targets [B1 B2]})
 (def ui-b-router (comp/factory BRouter))
 
-(defsc B [this {:keys [id router] :as props}]
-  {:query               [:id {:router (comp/get-query BRouter)}]
-   :ident               :id
+(defsc B [this {:keys [:b/id router] :as props}]
+  {:query               [:b/id {:router (comp/get-query BRouter)}]
+   :ident               :b/id
    :route-segment       ["b"]
    :will-enter          (fn [app params]
                           (log/info "B will enter")
-                          (dr/route-immediate [:id "b"]))
+                          (dr/route-immediate [:b/id "b"]))
    :will-leave          (fn [cls props] (log/info "B will leave"))
    :allow-route-change? (fn [c] (log/info "B allow route change?") true)
-   :initial-state       {:id "b" :router {}}}
+   :initial-state       {:b/id "b" :router {}}}
   (dom/div {}
     (dom/h2 "B")
-    (dom/button {:onClick (fn [] (dr/change-route-relative! this B ["b1"]))} "B1 (relative)")
-    (dom/button {:onClick (fn [] (dr/change-route-relative! this B ["b2"]))} "B2 (relative)")
+    (dom/button {:onClick (fn [] (dr/change-route-relative! this B ["b1"])) :className "router-button"} "B1 (relative)")
+    (dom/button {:onClick (fn [] (dr/change-route-relative! this B ["b2"])) :className "router-button"} "B2 (relative)")
     (ui-b-router router)))
 
-(def ui-b (comp/factory B {:keyfn :id}))
+(def ui-b (comp/factory B {:keyfn :b/id}))
 
-(defsc A [this {:keys [id router] :as props}]
-  {:query               [:id {:router (comp/get-query ARouter)}]
+(defsc A [this {:keys [:a/id router] :as props}]
+  {:query               [:a/id {:router (comp/get-query ARouter)}]
    :route-segment       ["a"]
    :will-enter          (fn [app params]
                           (log/info "A will enter")
-                          (dr/route-immediate [:id "a"]))
+                          (dr/route-immediate [:a/id "a"]))
    :will-leave          (fn [cls props] (log/info "A will leave"))
    :allow-route-change? (fn [c] (log/info "A allow route change?") true)
-   :initial-state       {:id "a" :router {}}
-   :ident               :id}
+   :initial-state       {:a/id "a" :router {}}
+   :ident               :a/id}
   (dom/div {}
     (dom/h2 "A")
-    (dom/button {:onClick (fn [] (dr/change-route-relative! this A ["a1"]))} "A1 (relative)")
-    (dom/button {:onClick (fn [] (dr/change-route-relative! this A ["a2"]))} "A2 (relative)")
+    (dom/button {:onClick (fn [] (dr/change-route-relative! this A ["a1"])) :className "router-button"} "A1 (relative)")
+    (dom/button {:onClick (fn [] (dr/change-route-relative! this A ["a2"])) :className "router-button"} "A2 (relative)")
     (ui-a-router router)))
 
-(def ui-a (comp/factory A {:keyfn :id}))
+(def ui-a (comp/factory A {:keyfn :a/id}))
 
 (defrouter ClassRootRouter [this {:keys [route-factory route-props]}]
   {:router-targets      [A B]
@@ -171,10 +175,10 @@
   [this {::keys [router relative-class-or-instance factory]}]
   {}
   (dom/div
-    (dom/button {:onClick (fn [] (dr/change-route-relative! this relative-class-or-instance ["a" "a1"]))} "A1")
-    (dom/button {:onClick (fn [] (dr/change-route-relative! this relative-class-or-instance ["b" "b2"]))} "B2")
-    (dom/button {:onClick (fn [] (dr/change-route-relative! this relative-class-or-instance ["a"]))} "A")
-    (dom/button {:onClick (fn [] (dr/change-route-relative! this relative-class-or-instance ["b"]))} "B")
+    (dom/button {:onClick (fn [] (dr/change-route-relative! this relative-class-or-instance ["a" "a1"])) :className "router-button"} "A1")
+    (dom/button {:onClick (fn [] (dr/change-route-relative! this relative-class-or-instance ["b" "b2"])) :className "router-button"} "B2")
+    (dom/button {:onClick (fn [] (dr/change-route-relative! this relative-class-or-instance ["a"])) :className "router-button"} "A")
+    (dom/button {:onClick (fn [] (dr/change-route-relative! this relative-class-or-instance ["b"])) :className "router-button"} "B")
     (factory router)))
 
 (def ui-navigation-and-router (comp/factory NavigationAndRouter))
@@ -214,26 +218,62 @@
   (dr/initialize! app)
   (dr/change-route! app ["a" "a1"]))
 
+(defn ensure-ident-can-be-derived
+  "Fulcro props middleware that ensures an ident can be derived, and if not, log a console.error"
+  [class js-props]
+  {:future-work "This is not the best place for this fn"
+   :related     'com.fulcrologic.fulcro.algorithms.indexing/index-component!}
+  (let [fulcro-value (gobj/get js-props "fulcro$value")
+        has-parent   (gobj/get js-props "fulcro$parent")]
+    (when (and
+            has-parent                                      ; Not Root and has a query
+            (comp/query class))
+      (let [ident (comp/ident class fulcro-value)]
+        (when-not ident
+          (js/console.group "Ident failed to derive")
+          (js/console.error (str "What is this, why can we not get an ident for it? " (comp/component-name class)))
+          (js/console.info (with-out-str (cljs.pprint/pprint {:className (comp/component-name class)
+                                                              :props     fulcro-value})))
+          (js/console.groupEnd))
+        (when (and
+                (and (seq fulcro-value))
+                (not (second ident)))
+          (js/console.group "Ident derived, but, with a `nil` for ident-v and we have props")
+          (js/console.error (str "What is this, why can we not get a valid ident for it? " (comp/component-name class)))
+          (js/console.info (with-out-str (cljs.pprint/pprint {:className (comp/component-name class)
+                                                              :ident     ident
+                                                              :props     fulcro-value})))
+          (js/console.groupEnd)))))
+  js-props)
+
+(defn make-app []
+  (com.fulcrologic.fulcro.cards.console-errors-display/init!
+    ; This needs to not-happen?
+    #{#"was called but there was no router waiting for the target"})
+  (merge (react18/react18-options)
+    {:client-will-mount client-will-mount
+     :props-middleware  ensure-ident-can-be-derived}))
+
 (ws/defcard nested-routing-class-demo
   (ct.fulcro/fulcro-card
     {::ct.fulcro/wrap-root? false
      ::ct.fulcro/root       ClassRoot
-     ::ct.fulcro/app        (merge (react18/react18-options) {:client-will-mount client-will-mount})}))
+     ::ct.fulcro/app        (make-app)}))
 
 (ws/defcard nested-routing-class-with-functional-router-demo
   (ct.fulcro/fulcro-card
     {::ct.fulcro/wrap-root? false
      ::ct.fulcro/root       ClassRootWithFunctionalRouter
-     ::ct.fulcro/app        (merge (react18/react18-options) {:client-will-mount client-will-mount})}))
+     ::ct.fulcro/app        (make-app)}))
 
 (ws/defcard nested-routing-hooks-demo
   (ct.fulcro/fulcro-card
     {::ct.fulcro/wrap-root? false
      ::ct.fulcro/root       HooksRoot
-     ::ct.fulcro/app        (merge (react18/react18-options) {:client-will-mount client-will-mount})}))
+     ::ct.fulcro/app        (make-app)}))
 
 (ws/defcard nested-routing-hooks-with-functional-router-demo
   (ct.fulcro/fulcro-card
     {::ct.fulcro/wrap-root? false
      ::ct.fulcro/root       HooksRootWithFunctionalRouter
-     ::ct.fulcro/app        (merge (react18/react18-options) {:client-will-mount client-will-mount})}))
+     ::ct.fulcro/app        (make-app)}))
